d55db0126f3f436935c1959a670a00c8872d0b27
deser56 <86250972+deser56@users.noreply.github.com>
2023-06-12 19:27:54 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -13,8 +13,12 @@ import {
   MenuItem,
   Avatar,
   Stack,
+  Box,
+  createTheme,
+  ThemeProvider,
+  useMediaQuery,
 } from '@mui/material';
-import { styled, createTheme, ThemeProvider, useMediaQuery } from '@mui/material';
+import { styled } from '@mui/system';
 import MenuIcon from '@mui/icons-material/Menu';
 import ConnectWallet from './connect';
 import NFTMintComponent from './mintf';
@@ -25,14 +29,18 @@ const gradientLight = `linear-gradient(to bottom right, ${primaryColor}, #C5E1A5
 const gradientDark = `linear-gradient(to bottom right, #558B2F, ${primaryColor})`;
 
 // Styled components for customized styling
-const StyledContainer = styled('div')({
+const StyledContainer = styled('div')(({ theme }) => ({
   backgroundColor: '#f7f7f7',
   padding: '24px',
   minHeight: '100vh',
   display: 'flex',
   alignItems: 'center', // Center the components vertically
   justifyContent: 'center', // Center the components horizontally
-});
+  [theme.breakpoints.up('md')]: {
+    margin: '0 auto', // Center the container horizontally on larger screens
+    maxWidth: '960px', // Limit the width of the container on larger screens
+  },
+}));
 
 const StyledCard = styled(Card)({
   maxWidth: 400,
@@ -42,37 +50,12 @@ const StyledCard = styled(Card)({
   borderRadius: '16px',
 });
 
-const StyledButton = styled(Button)({
-  background: gradientDark,
-  color: '#fff',
-  '&:hover': {
-    background: gradientLight,
-  },
-  borderRadius: '24px',
-  padding: '12px 24px',
-  textTransform: 'capitalize',
-});
-
-const StyledAppBar = styled(AppBar)({
-  backgroundColor: primaryColor,
-  boxShadow: 'none',
-});
-
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
-  },
-});
+const theme = createTheme();
 
 const App = () => {
   const [numNFTs, setNumNFTs] = useState(1);
   const [anchorEl, setAnchorEl] = useState(null);
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleNumNFTsChange = (event) => {
     setNumNFTs(event.target.value);
@@ -92,73 +75,69 @@ const App = () => {
     setAnchorEl(null);
   };
 
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
   return (
     <ThemeProvider theme={theme}>
-      <Stack direction="column" height={isMobile ? "100%" : "100vh"}>
-        <StyledAppBar position="static">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={handleMenuOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              jungleking
-            </Typography>
-            <IconButton color="inherit">
-              <Avatar alt="User Avatar" src="path/to/avatar.jpg" />
-            </IconButton>
-          </Toolbar>
-        </StyledAppBar>
+      <AppBar position="static" sx={{ boxShadow: 'none', backgroundColor: primaryColor }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            jungleking
+          </Typography>
+          <IconButton color="inherit">
+            <Avatar alt="User Avatar" src="path/to/avatar.jpg" />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
 
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-          <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
-        </Menu>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Settings</MenuItem>
+      </Menu>
 
-        <StyledContainer>
-          <Stack spacing={4}>
-            <ConnectWallet />
-            <Typography variant="h4" gutterBottom>
-              NFT Preview
-            </Typography>
-            <StyledCard>
-              <CardMedia
-                component="img"
-                height="200"
-                image="path/to/nft-preview-image.jpg"
-                alt="NFT Preview"
-                style={{ borderRadius: '16px 16px 0 0' }}
-              />
-              <CardContent>
-                <Typography variant="body1">nftz</Typography>
-              </CardContent>
-            </StyledCard>
+      <StyledContainer>
+        <Stack spacing={4} sx={{ width: '100%' }}>
+          <ConnectWallet />
+          <Typography variant="h4" gutterBottom>
+            NFT Preview
+          </Typography>
+          <StyledCard>
+            <CardMedia
+              component="img"
+              height="200"
+              image="path/to/nft-preview-image.jpg"
+              alt="NFT Preview"
+              style={{ borderRadius: '16px 16px 0 0' }}
+            />
+            <CardContent>
+              <Typography variant="body1">nftz</Typography>
+            </CardContent>
+          </StyledCard>
 
-            <Typography variant="h4" gutterBottom>
-              Mint NFTs
-            </Typography>
-            <NFTMintComponent />
+          <Typography variant="h4" gutterBottom>
+            Mint NFTs
+          </Typography>
+          <NFTMintComponent />
 
-           
-          </Stack>
-        </StyledContainer>
-      </Stack>
+        </Stack>
+      </StyledContainer>
     </ThemeProvider>
   );
 };
 
 export default App;
+
 
 
