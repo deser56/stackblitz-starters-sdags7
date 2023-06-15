e26787347b469d492f9c55f954901f5fab95ee81
deser56 <86250972+deser56@users.noreply.github.com>
2023-06-15 22:10:09 +0300

    1 file modified

--- src/ethy.js
+++ src/ethy.js
@@ -82,7 +82,7 @@ function EthereumButton() {
       console.error('No Ethereum wallet provider found.');
       // Display a modal or popup with instructions for installing a wallet
       // For example, using a custom modal
-      displayCustomModal();
+      // displayCustomModal();
     } else {
       // Ethereum wallet provider exists
       // window.web3 = new Web3(window.ethereum);
