d9eefcafde6fa4058748b3d6c0edc5ccb4ecadcc
deser56 <86250972+deser56@users.noreply.github.com>
2023-06-15 21:41:08 +0300

    1 file modified

--- src/ethy.js
+++ src/ethy.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Modal, Card, CardContent, Link, Typography, ThemeProvider, createTheme } from '@mui/material';
-import Web3 from 'web3';
 import { styled } from '@mui/styles';
 
 const theme = createTheme({
@@ -63,7 +62,7 @@ const CardContainer = styled('div')({
   margin: '1rem',
 });
 
-export let isWalletConnected = false;
+export let isWalletConnected = false; // Export the variable
 
 function EthereumButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -77,24 +76,34 @@ function EthereumButton() {
   };
 
   useEffect(() => {
+    // Check if Ethereum wallet provider exists
     if (!window.ethereum) {
+      // Ethereum wallet provider is not installed
       console.error('No Ethereum wallet provider found.');
+      // Display a modal or popup with instructions for installing a wallet
+      // For example, using a custom modal
       displayCustomModal();
     } else {
-      window.web3 = new Web3(window.ethereum);
+      // Ethereum wallet provider exists
+      // window.web3 = new Web3(window.ethereum);
+      // setIsWalletConnected(true);
     }
   }, []);
 
   const handleSignIn = async () => {
     try {
-      const accounts = await window.ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-
-      const account = accounts[0];
-      console.log('Signed in with Ethereum wallet:', account);
-      handleCloseModal();
-      isWalletConnected = true;
+      if (window.ethereum && window.ethereum.request) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+        console.log('Signed in with Ethereum wallet:', accounts[0]);
+        handleCloseModal();
+        // Perform any necessary actions after signing in
+        isWalletConnected = true;
+      } else {
+        console.error('Ethereum wallet provider not available.');
+        // Handle error
+      }
     } catch (error) {
       console.error('Error signing in with Ethereum wallet:', error);
       // Handle error
@@ -168,5 +177,6 @@ function EthereumButton() {
 }
 
 export default EthereumButton;
+
 
 
