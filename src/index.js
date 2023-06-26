578fcc01da08a9ba79dcd1ef643706e8a9e0db68
deser56 <86250972+deser56@users.noreply.github.com>
2023-06-26 08:26:12 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -7,23 +7,9 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 
-function Disp () {
-  try {
-  // Your code here that may cause the BigInt to number conversion error
-  return (<App />);
-} catch (error) {
-  if (error instanceof TypeError && error.message.includes('Cannot convert a BigInt value to a number')) {
-    // Handle the specific error gracefully
-    console.error('Error: Cannot convert a BigInt value to a number. Please check your code for any potential BigInt to number conversions.');
-  } else {
-    // Handle other errors
-    console.error('An error occurred:', error);
-  }
-}
-}
 
 root.render(
   <StrictMode>
-    <Disp />
+    <App />
   </StrictMode>
 );
